4edc6e5f03ec5b62663c5cd83829462ea454d35c
pinkxpink00 <kyperu07@gmail.com>
2024-01-03 23:23:26 +0200

    initial EventHandler

---
+++ Events/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main()
+    {
+        EventCounter counter = new EventCounter();
+        FirstHandler handler1 = new FirstHandler();
+        SecondHandler handler2 = new SecondHandler();
+
+        counter.onEventCount += handler1.Message;
+        counter.onEventCount += handler2.Message;
+
+        counter.eventCount();
+    }
+}
+
+class EventCounter
+{
+    public delegate void MethodContainer();
+
+    public event MethodContainer onEventCount;
+    public void eventCount()
+    {
+      
+        for (int i = 0; i < 100; i++)
+        {
+            bool isCheck71 = (i == 71);
+
+            if (isCheck71)
+            {
+                onEventCount();
+            }
+        }
+    }
+}
+
+class FirstHandler
+{
+    public void Message()
+    {
+        string stringMessage = "Пора действовать, ведь уже 71";
+        Console.WriteLine(stringMessage);
+    }
+}
+
+class SecondHandler
+{
+    public void Message()
+    {
+        string stringMessage = "Точно, уже 71";
+        Console.WriteLine(stringMessage);
+    }
+}
