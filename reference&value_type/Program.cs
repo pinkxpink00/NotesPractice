1a7e3e73dac103516d7b1edc9c4f26eef0798e28
pinkxpink00 <kyperu07@gmail.com>
2024-01-04 23:04:56 +0200

    v2

--- reference&value_type/Program.cs
+++ reference&value_type/Program.cs
@@ -2,7 +2,12 @@
 {
     static void Main(string[] args)
     {
-        //Размещение в стеке, потому что int == struct type
+        //Размещение в стеке, потому что int == struct (value types)
         int num1 = 0;
+
+        {
+            //Размещение в куче, потому что array == class (reference types)
+            int[] arrayNum2 = new int[2];
+        }
     }
 }
