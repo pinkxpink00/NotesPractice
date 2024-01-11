ac2b0ee5dfc63ff06fbc1ebda7fdba26f1d12f79
pinkxpink00 <kyperu07@gmail.com>
2024-01-11 16:34:30 +0200

    abstract vol 1

---
+++ Interface and Polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Ship ship = new Ship();
+        ship.Move();
+    }
+}
+
+class Ship : Transport
+{
+
+}
+
+abstract class Transport
+{
+    public void Move()
+    {
+        Console.WriteLine("Transport moves");
+    }
+}
