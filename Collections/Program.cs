5be534af6a0a9ac97dababd5b3303451a366027c
pinkxpink00 <kyperu07@gmail.com>
2024-01-12 22:45:39 +0200

    Dictionary

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -2,18 +2,35 @@
 {
     static void Main()
     {
-        var people = new List<string>() {"Loice","Capy"};
+        //var people = new List<string>() {"Loice","Capy"};
 
-        people.Add("Joahna");
-        people.Add("Tom");
+        //people.Add("Joahna");
+        //people.Add("Tom");
 
-        var firstPeople = people[2];
+        //var firstPeople = people[2];
 
-        Console.WriteLine(firstPeople);
+        //Console.WriteLine(firstPeople);
 
-        foreach (var person in people)
+        //foreach (var person in people)
+        //{
+        //    Console.WriteLine(person);
+        //}
+
+        var ages = new Dictionary<string, int>();
+
+        ages["Alice"]= 30;
+        ages["ASD"] = 305;
+        ages["asdasd"] = 4;
+        ages["Alicfasfasfe"] = 3;
+
+
+
+        var alice = ages["Alice"];
+
+        foreach (var age in ages)
         {
-            Console.WriteLine(person);
+            Console.WriteLine($"{age.Key} has {age.Value} yo");
         }
+
     }
 }
