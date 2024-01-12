a55f40c4682bd2efaf6572facca373bc206d5344
pinkxpink00 <kyperu07@gmail.com>
2024-01-12 22:24:57 +0200

    List Collection

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var people = new List<string>() {"Loice","Capy"};
+
+        people.Add("Joahna");
+        people.Add("Tom");
+
+        var firstPeople = people[2];
+
+        Console.WriteLine(firstPeople);
+
+        foreach (var person in people)
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
