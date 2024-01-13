b45f284db5910968f348cb84dad52b6f9003b052
pinkxpink00 <kyperu07@gmail.com>
2024-01-13 22:08:43 +0200

    interface v1

---
+++ Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+interface IMovable
+{
+    void Move();
+
+    string Name { get; set; }
+
+    const int minSpeed = 0;
+    static int maxSpeed = 100;
+
+    delegate void MoveHandler(string message);
+
+    event MoveHandler MoveEvent;
+}
