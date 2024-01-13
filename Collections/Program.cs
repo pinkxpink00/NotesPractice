97fa47c30bc60399aac465321079c690ea9c4227
pinkxpink00 <kyperu07@gmail.com>
2024-01-13 21:37:22 +0200

    ObservableCollectionv1

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,7 +1,20 @@
-﻿class Program
+﻿using System.Collections.ObjectModel;
+
+class Program
 {
     static void Main()
     {
+        var e = new ObservableCollection<int>();
+
+        e.Add(1);
+        e.Add(2);
+        e.Add(3);
+
+        foreach (var item in e)
+        {
+            Console.WriteLine(item);
+        }
+
         //var people = new List<string>() {"Loice","Capy"};
 
         //people.Add("Joahna");
@@ -16,21 +29,21 @@
         //    Console.WriteLine(person);
         //}
 
-        var ages = new Dictionary<string, int>();
+        //var ages = new Dictionary<string, int>();
 
-        ages["Alice"]= 30;
-        ages["ASD"] = 305;
-        ages["asdasd"] = 4;
-        ages["Alicfasfasfe"] = 3;
+        //ages["Alice"]= 30;
+        //ages["ASD"] = 305;
+        //ages["asdasd"] = 4;
+        //ages["Alicfasfasfe"] = 3;
 
 
 
-        var alice = ages["Alice"];
+        //var alice = ages["Alice"];
 
-        foreach (var age in ages)
-        {
-            Console.WriteLine($"{age.Key} has {age.Value} yo");
-        }
+        //foreach (var age in ages)
+        //{
+        //    Console.WriteLine($"{age.Key} has {age.Value} yo");
+        //}
 
     }
 }
